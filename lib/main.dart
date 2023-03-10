3e500e04b5709cd3aaa2dbd675b85527f0961730
DASHU <385321165@qq.com>
2023-03-10 12:27:06 +0800

    logo、flutter_native_splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:nostr_dart/nostr_dart.dart';
 import 'package:nostrmo/data/db.dart';
 import 'package:provider/provider.dart';
@@ -48,6 +49,7 @@ Future<void> main() async {
   // }
   nostr = genNostr(generatePrivateKey());
 
+  FlutterNativeSplash.remove();
   runApp(MyApp());
 }
 
