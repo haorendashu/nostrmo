ebaed338ab528a0ad5901089fcda4ca0580b3e20
DASHU <385321165@qq.com>
2023-03-25 01:15:21 +0800

    DMDetailRouter add a btn to write a message

--- lib/router/dm/dm_detail_router.dart
+++ lib/router/dm/dm_detail_router.dart
@@ -2,12 +2,15 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:nostrmo/client/dm_session.dart';
 import 'package:nostrmo/component/name_component.dart';
+import 'package:nostrmo/consts/base.dart';
 import 'package:nostrmo/router/dm/dm_detail_item_component.dart';
+import 'package:nostrmo/router/index/index_app_bar.dart';
 import 'package:nostrmo/util/router_util.dart';
 import 'package:provider/provider.dart';
 import 'package:pointycastle/export.dart' as pointycastle;
 
 import '../../client/nip04/nip04.dart';
+import '../../consts/router_path.dart';
 import '../../data/metadata.dart';
 import '../../main.dart';
 import '../../provider/metadata_provider.dart';
@@ -46,8 +49,10 @@ class _DMDetailRouter extends State<DMDetailRouter> {
       },
     );
 
-    var localPubkey = nostr!.publicKey;
+    var themeData = Theme.of(context);
+    var hintColor = themeData.hintColor;
 
+    var localPubkey = nostr!.publicKey;
     var agreement = NIP04.getAgreement(nostr!.privateKey);
 
     return Scaffold(
@@ -62,24 +67,50 @@ class _DMDetailRouter extends State<DMDetailRouter> {
       ),
       body: Container(
         width: double.maxFinite,
-        child: ListView.builder(
-          itemBuilder: (context, index) {
-            var event = dmSession!.get(index);
-            if (event == null) {
-              return null;
-            }
+        child: Column(children: [
+          Expanded(
+              child: ListView.builder(
+            itemBuilder: (context, index) {
+              var event = dmSession!.get(index);
+              if (event == null) {
+                return null;
+              }
 
-            return DMDetailItemComponent(
-              sessionPubkey: dmSession!.pubkey,
-              event: event,
-              isLocal: localPubkey == event.pubKey,
-              agreement: agreement,
-            );
-          },
-          reverse: true,
-          itemCount: dmSession!.length(),
-          dragStartBehavior: DragStartBehavior.down,
-        ),
+              return DMDetailItemComponent(
+                sessionPubkey: dmSession!.pubkey,
+                event: event,
+                isLocal: localPubkey == event.pubKey,
+                agreement: agreement,
+              );
+            },
+            reverse: true,
+            itemCount: dmSession!.length(),
+            dragStartBehavior: DragStartBehavior.down,
+          )),
+          GestureDetector(
+            onTap: () {
+              RouterUtil.router(context, RouterPath.EDITOR);
+            },
+            child: Container(
+              margin: EdgeInsets.only(
+                left: Base.BASE_PADDING,
+                top: Base.BASE_PADDING_HALF,
+                right: Base.BASE_PADDING,
+                bottom: Base.BASE_PADDING_HALF,
+              ),
+              height: 40,
+              alignment: Alignment.center,
+              // color: Colors.black,
+              color: hintColor,
+              child: Text(
+                "Write a message",
+                style: TextStyle(
+                    // color: Colors.white,
+                    ),
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
