b53ac3b3ba889bfb0df2bb2f85902497039c6a3b
DASHU <385321165@qq.com>
2023-04-05 19:55:33 +0800

    zaping add loading

--- lib/client/zap/zap_action.dart
+++ lib/client/zap/zap_action.dart
@@ -9,40 +9,45 @@ import 'zap.dart';
 class ZapAction {
   static Future<void> handleZap(BuildContext context, int sats, String pubkey,
       {String? eventId}) async {
-    var metadata = metadataProvider.getMetadata(pubkey);
-    if (metadata == null) {
-      BotToast.showText(text: "Metadata can not be found.");
-      return;
-    }
-
-    var relays = relayProvider.relayAddrs;
-
-    String? lnurl = metadata.lud06;
-    if (StringUtil.isBlank(lnurl)) {
-      if (StringUtil.isNotBlank(metadata.lud16)) {
-        lnurl = Zap.getLnurlFromLud16(metadata.lud16!);
+    var cancelFunc = BotToast.showLoading();
+    try {
+      var metadata = metadataProvider.getMetadata(pubkey);
+      if (metadata == null) {
+        BotToast.showText(text: "Metadata can not be found.");
+        return;
       }
+
+      var relays = relayProvider.relayAddrs;
+
+      String? lnurl = metadata.lud06;
+      if (StringUtil.isBlank(lnurl)) {
+        if (StringUtil.isNotBlank(metadata.lud16)) {
+          lnurl = Zap.getLnurlFromLud16(metadata.lud16!);
+        }
+      }
+
+      if (StringUtil.isBlank(lnurl)) {
+        BotToast.showText(text: "Gen lnurl error.");
+        return;
+      }
+
+      var invoiceCode = await Zap.getInvoiceCode(
+        lnurl: lnurl!,
+        sats: sats,
+        recipientPubkey: pubkey,
+        targetNostr: nostr!,
+        relays: relays,
+        eventId: eventId,
+      );
+
+      if (StringUtil.isBlank(invoiceCode)) {
+        BotToast.showText(text: "Gen invoiceCode error.");
+        return;
+      }
+
+      await LightningUtil.goToPay(invoiceCode!);
+    } finally {
+      cancelFunc.call();
     }
-
-    if (StringUtil.isBlank(lnurl)) {
-      BotToast.showText(text: "Gen lnurl error.");
-      return;
-    }
-
-    var invoiceCode = await Zap.getInvoiceCode(
-      lnurl: lnurl!,
-      sats: sats,
-      recipientPubkey: pubkey,
-      targetNostr: nostr!,
-      relays: relays,
-      eventId: eventId,
-    );
-
-    if (StringUtil.isBlank(invoiceCode)) {
-      BotToast.showText(text: "Gen invoiceCode error.");
-      return;
-    }
-
-    await LightningUtil.goToPay(invoiceCode!);
   }
 }
