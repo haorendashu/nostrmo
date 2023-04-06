d2f30ba3f707ae7356c45610efd7add37f28cf03
DASHU <385321165@qq.com>
2023-04-06 23:18:05 +0800

    webview add open in browser

--- lib/component/webview_router.dart
+++ lib/component/webview_router.dart
@@ -1,7 +1,11 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:nostrmo/component/cust_state.dart';
 import 'package:nostrmo/consts/base.dart';
 import 'package:nostrmo/util/router_util.dart';
+import 'package:nostrmo/util/string_util.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 import '../main.dart';
@@ -39,6 +43,8 @@ class _WebViewRouter extends CustState<WebViewRouter> {
     var paddingTop = mediaDataCache.padding.top;
     var scaffoldBackgroundColor = themeData.scaffoldBackgroundColor;
 
+    var btnTopPosition = Base.BASE_PADDING + Base.BASE_PADDING_HALF;
+
     return Scaffold(
       body: Container(
         margin: EdgeInsets.only(top: paddingTop),
@@ -49,7 +55,7 @@ class _WebViewRouter extends CustState<WebViewRouter> {
             ),
             Positioned(
               left: Base.BASE_PADDING,
-              top: Base.BASE_PADDING + Base.BASE_PADDING_HALF,
+              top: btnTopPosition,
               child: GestureDetector(
                 onTap: () {
                   RouterUtil.back(context);
@@ -66,6 +72,39 @@ class _WebViewRouter extends CustState<WebViewRouter> {
                 ),
               ),
             ),
+            Positioned(
+              right: Base.BASE_PADDING,
+              top: btnTopPosition,
+              child: PopupMenuButton<String>(
+                itemBuilder: (context) {
+                  return [
+                    PopupMenuItem(
+                      value: "copyCurrentUrl",
+                      child: Text("Copy current Url"),
+                    ),
+                    PopupMenuItem(
+                      value: "copyInitUrl",
+                      child: Text("Copy init Url"),
+                    ),
+                    PopupMenuItem(
+                      value: "openInBrowser",
+                      child: Text("Open in browser"),
+                    ),
+                  ];
+                },
+                onSelected: onPopupSelected,
+                child: Container(
+                  height: btnWidth,
+                  width: btnWidth,
+                  decoration: BoxDecoration(
+                    color: scaffoldBackgroundColor,
+                    borderRadius: BorderRadius.circular(btnWidth / 2),
+                  ),
+                  child: Icon(Icons.more_horiz),
+                  alignment: Alignment.center,
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -76,5 +115,25 @@ class _WebViewRouter extends CustState<WebViewRouter> {
   Future<void> onReady(BuildContext context) async {
     _controller.setJavaScriptMode(JavaScriptMode.unrestricted);
     _controller.loadRequest(Uri.parse(widget.url));
+  }
+
+  Future<void> onPopupSelected(String value) async {
+    var url = await _controller.currentUrl();
+    if (value == "copyCurrentUrl") {
+      if (StringUtil.isNotBlank(url)) {
+        _doCopy(url!);
+      }
+    } else if (value == "copyInitUrl") {
+      _doCopy(widget.url);
+    } else if (value == "openInBrowser") {
+      var _url = Uri.parse(widget.url);
+      launchUrl(_url);
+    }
+  }
+
+  void _doCopy(String text) {
+    Clipboard.setData(ClipboardData(text: text)).then((_) {
+      BotToast.showText(text: "Copy success!");
+    });
   }
 }
