b9d760ee967db2b4fcfc23fbb23a4294be817537
DASHU <385321165@qq.com>
2023-07-21 13:59:41 +0800

    nostrbuild change

--- lib/client/upload/nostr_build_uploader.dart
+++ lib/client/upload/nostr_build_uploader.dart
@@ -1,12 +1,37 @@
 import 'package:dio/dio.dart';
-import 'package:nostrmo/util/spider_util.dart';
 
 import '../../consts/base64.dart';
 
 class NostrBuildUploader {
   static var dio = Dio();
 
-  static final String UPLOAD_ACTION = "https://nostr.build/upload.php";
+  // static final String UPLOAD_ACTION = "https://nostr.build/upload.php";
+
+  // static Future<String?> upload(String filePath, {String? fileName}) async {
+  //   MultipartFile? multipartFile;
+  //   if (BASE64.check(filePath)) {
+  //     var bytes = BASE64.toData(filePath);
+  //     multipartFile = await MultipartFile.fromBytes(
+  //       bytes,
+  //       filename: fileName,
+  //     );
+  //   } else {
+  //     multipartFile = await MultipartFile.fromFile(
+  //       filePath,
+  //       filename: fileName,
+  //     );
+  //   }
+
+  //   var formData = FormData.fromMap({"fileToUpload": multipartFile});
+  //   var response = await dio.post<String>(UPLOAD_ACTION, data: formData);
+  //   var body = response.data;
+  //   // TODO this rule need to update by api
+  //   var uploadResult = SpiderUtil.subUntil(body!, "<a id=\"theList\">", "</a>");
+
+  //   return uploadResult;
+  // }
+
+  static final String UPLOAD_ACTION = "https://nostr.build/api/v2/upload/files";
 
   static Future<String?> upload(String filePath, {String? fileName}) async {
     MultipartFile? multipartFile;
@@ -23,12 +48,13 @@ class NostrBuildUploader {
       );
     }
 
-    var formData = FormData.fromMap({"fileToUpload": multipartFile});
-    var response = await dio.post<String>(UPLOAD_ACTION, data: formData);
+    var formData = FormData.fromMap({"file": multipartFile});
+    var response = await dio.post(UPLOAD_ACTION, data: formData);
     var body = response.data;
-    // TODO this rule need to update by api
-    var uploadResult = SpiderUtil.subUntil(body!, "<a id=\"theList\">", "</a>");
+    if (body is Map<String, dynamic> && body["status"] == "success") {
+      return body["data"][0]["url"];
+    }
 
-    return uploadResult;
+    return null;
   }
 }
