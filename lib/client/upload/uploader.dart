a7da410965c33ff0b991ad8e6564ff317c926b35
dashu <385321165@qq.com>
2024-04-28 09:57:19 +0800

    ios macos build file

--- lib/client/upload/uploader.dart
+++ lib/client/upload/uploader.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
--- lib/consts/base.dart
+++ lib/consts/base.dart
@@ -3,7 +3,7 @@ import 'package:nostrmo/util/hash_util.dart';
 class Base {
   static const APP_NAME = "Nostrmo";
 
-  static String VERSION_NAME = "2.4.0";
+  static String VERSION_NAME = "2.5.0";
 
   static int VERSION_NUM = 1;
 
