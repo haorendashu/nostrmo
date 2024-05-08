1c8454a95c4a9fb467d3a096086c537981c96135
DASHU <385321165@qq.com>
2024-05-08 17:16:14 +0800

    contact set clear when logout

--- lib/provider/contact_list_provider.dart
+++ lib/provider/contact_list_provider.dart
@@ -201,6 +201,7 @@ class ContactListProvider extends ChangeNotifier {
     _contactList!.clear();
     content = "";
     clearCurrentContactList();
+    followSetMap.clear();
 
     notifyListeners();
   }
