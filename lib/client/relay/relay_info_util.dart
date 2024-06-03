76227ea6d333a116f436d554e38371ced79b5de0
DASHU <385321165@qq.com>
2024-06-04 01:52:08 +0800

    add error catch to get relay info method

--- lib/client/relay/relay_info_util.dart
+++ lib/client/relay/relay_info_util.dart
@@ -3,16 +3,23 @@ import 'package:http/http.dart' as http;
 import 'relay_info.dart';
 
 class RelayInfoUtil {
-  static Future<RelayInfo> get(String url) async {
+  static Future<RelayInfo?> get(String url) async {
     late Uri uri;
     if (url.indexOf("wss://") == 0) {
       uri = Uri.parse(url).replace(scheme: 'https');
     } else {
       uri = Uri.parse(url).replace(scheme: 'http');
     }
-    final response =
-        await http.get(uri, headers: {'Accept': 'application/nostr+json'});
-    final decodedResponse = jsonDecode(response.body) as Map;
-    return RelayInfo.fromJson(decodedResponse);
+    try {
+      final response =
+          await http.get(uri, headers: {'Accept': 'application/nostr+json'});
+      final decodedResponse = jsonDecode(response.body) as Map;
+      return RelayInfo.fromJson(decodedResponse);
+    } catch (e) {
+      print("RelayInfo get error:");
+      print(e);
+    }
+
+    return null;
   }
 }
