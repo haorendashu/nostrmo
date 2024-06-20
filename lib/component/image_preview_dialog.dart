99a2f8c6adf76603f1aada90dea3adceab3691f0
DASHU <385321165@qq.com>
2024-06-20 14:13:19 +0800

    image preview tooltip change

--- lib/component/image_preview_dialog.dart
+++ lib/component/image_preview_dialog.dart
@@ -130,6 +130,8 @@ class _ImagePreviewDialog extends State<ImagePreviewDialog> {
 
   @override
   Widget build(BuildContext context) {
+    var s = S.of(context);
+
     var main = Scaffold(
       backgroundColor: widget.backgroundColor.withOpacity(0.5),
       body: Stack(
@@ -165,7 +167,7 @@ class _ImagePreviewDialog extends State<ImagePreviewDialog> {
                 child: IconButton(
                   icon: const Icon(Icons.download),
                   color: widget.closeButtonColor,
-                  tooltip: widget.closeButtonTooltip,
+                  tooltip: s.Download,
                   onPressed: saveImage,
                 )),
           ]),
