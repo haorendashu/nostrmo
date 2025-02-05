c18cf1a52c0dee742102ab518db5d5d61b7214d3
DASHU <385321165@qq.com>
2025-02-05 20:48:15 +0800

    change version name

--- lib/consts/base.dart
+++ lib/consts/base.dart
@@ -3,7 +3,7 @@ import 'package:nostrmo/util/hash_util.dart';
 class Base {
   static const APP_NAME = "Nostrmo";
 
-  static String VERSION_NAME = "2.9.5";
+  static String VERSION_NAME = "2.9.6";
 
   static int VERSION_NUM = 1;
 
