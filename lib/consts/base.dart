f372bab514813cbdf3400050126403b51780c76a
DASHU <385321165@qq.com>
2025-04-22 22:40:14 +0800

    ios macos build file

--- lib/consts/base.dart
+++ lib/consts/base.dart
@@ -3,7 +3,7 @@ import 'package:nostrmo/util/hash_util.dart';
 class Base {
   static const APP_NAME = "Nostrmo";
 
-  static String VERSION_NAME = "2.9.6";
+  static String VERSION_NAME = "3.0.0";
 
   static int VERSION_NUM = 1;
 
