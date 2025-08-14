02aecaecaa8f83063271a75007970263f31d3268
DASHU <385321165@qq.com>
2025-08-14 15:34:47 +0800

    pfm image add preview

--- lib/router/dm/dm_pfm_component.dart
+++ lib/router/dm/dm_pfm_component.dart
@@ -1,10 +1,14 @@
 import 'dart:typed_data';
 
+import 'package:easy_image_viewer/easy_image_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:nostr_sdk/event.dart';
 import 'package:nostr_sdk/nip17/pfm_algorithm_decrypt.dart';
 import 'package:nostrmo/component/cust_state.dart';
 import 'package:nostrmo/util/dio_util.dart';
+
+import '../../component/image_preview_dialog.dart';
+import '../../generated/l10n.dart';
 
 class DMPfmComponent extends StatefulWidget {
   Event event;
@@ -27,7 +31,21 @@ class _DMPfmComponentState extends CustState<DMPfmComponent> {
       return Container();
     }
 
-    return Image.memory(imageData!);
+    return GestureDetector(
+      onTap: () {
+        if (imageData != null) {
+          List<ImageProvider> imageProviders = [];
+          imageProviders.add(MemoryImage(imageData!));
+
+          MultiImageProvider multiImageProvider =
+              MultiImageProvider(imageProviders, initialIndex: 0);
+
+          ImagePreviewDialog.show(context, multiImageProvider);
+        }
+      },
+      behavior: HitTestBehavior.translucent,
+      child: Image.memory(imageData!),
+    );
   }
 
   @override
